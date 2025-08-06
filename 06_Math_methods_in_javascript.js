e1a172b23c26904f48a8c2ba8888ba8ee5edbdc8
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2025-08-07 03:45:59 +0530

    update in 5 or 6 chapter
    updated the file name for chapter 5,
    updated in the code for chapter 6.

---
+++ 06_Math_methods_in_javascript.js
@@ -0,0 +1,53 @@
+//  1. Addition (+)
+// let a = 10;
+// let b = 5;
+// console.log(a + b); // Output: 15
+
+// 2. Subtraction (-)
+// console.log(a-b);
+
+// 3. Multiplication (*)
+// console.log(a*b);
+
+// 4. Division (/)
+// console.log(a/b);
+
+//  5. Modulus (%)
+// console.log(a % b); // Output: 0
+// console.log(7%3);
+
+// 6. Exponentiation (**)
+// console.log(2 ** 3);
+
+// Math Object in javascript 
+// properties on Math Object
+
+// 1.Math.PI
+// console.log(Math.PI);  Output: 3.141592653589793
+// 2.Math.SQRT2
+// console.log(Math.SQRT2); Output : 1.4142135623730951
+// 3.Math.E
+// console.log(Math.E);  Output: 2.718281828459045
+
+// Methods on Math Object
+// 1.Math.sqrt(x) 
+    // Math.sqrt(number);
+    // console.log(Math.sqrt(9)); Output:3
+    // console.log(Math.sqrt(25)); Output:5
+    // console.log(Math.sqrt(0)); Output:0
+    // console.log(Math.sqrt(1)); Output:1
+
+// 2.Math.pow(x,y);
+    // Math.pow(Base,exponent);
+    // console.log(Math.pow(2,3)); Output: 8 (2³);
+    // console.log(Math.pow(5,2)); Output; 25 (5²);
+    // console.log(Math.pow(10,0)); Output: 1 (any number to the power of 0 is 1);
+    // console.log(Math.pow(4,-1)); Output: 0.25 (1/4);
+
+// 3.Math.floor(x);
+// The Math.floor() method rounds a number down to the nearest integer — always towards negative infinity.
+    // console.log(Math.floor(4.9)); output: 4
+    // console.log(Math.floor(4.1)); output: 4
+    // console.log(Math.floor(4)); output: 4
+    // console.log(Math.floor(-4.9)); output: -5
+    // console.log(Math.floor(-4.1)); output: -5
