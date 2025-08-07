2f9ef9980b4673efbba12aded6ea990d1202575f
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2025-08-07 13:17:31 +0530

    renaming the 2nd chapter
    just changing the name of 2 chapter

--- 02_variables_in_javascript.js
+++ 02_variables_in_javascript.js
@@ -10,62 +10,62 @@
 //  3.Using var keyword 
 // ====================================================================================================================================================
 //  we can also intitalize the variable after decalartion or at the time of decalartion
-let username = "Saksham";
-console.log(username);
+// let username = "Saksham";
+// console.log(username);
 // ->"Saksham"
-let userIntro = "My name is Anurag Singh";
-console.log(userIntro);
+// let userIntro = "My name is Anurag Singh";
+// console.log(userIntro);
 //->"My name is Anurag Singh"
-let age = 25
-console.log(age);
+// let age = 25
+// console.log(age);
 // ->25
-let isHappy = true;
-console.log(isHappy);
+// let isHappy = true;
+// console.log(isHappy);
 // ->true
 
 
-let userfile = username;
-console.log(userfile);
+// let userfile = username;
+// console.log(userfile);
 // ->Saksham
 
-let userNamefull = "hi my name is " + username + "       " + userIntro  + "my age is " +age ;
-console.log(userNamefull);
+// let userNamefull = "hi my name is " + username + "       " + userIntro  + "my age is " +age ;
+// console.log(userNamefull);
 // ->hi my name is Saksham       My name is Anurag Singh
 
-let a ;
-console.log(a);
+// let a ;
+// console.log(a);
 
 // ===================================================================================================================================================
 // -> const variable must be initialize.
-const hoursInDay = 24;
-console.log(hoursInDay);
+// const hoursInDay = 24;
+// console.log(hoursInDay);
 // hoursInDay = 18;
 // console.log(hoursInDay);
 // -> const variables can not be reassigned
 // ===================================================================================================================================================
 
-{
-    var variable1 = "saksham";
-}
+// {
+//     var variable1 = "saksham";
+// }
 
-{
-    let variable2 = "abhishek";
-}
+// {
+//     let variable2 = "abhishek";
+// }
 
-console.log(variable1);
+// console.log(variable1);
 // console.log(variable2); let and const varaible can not be accessible out the block scope while var varaible accessable outside,
 // the block scope if it not the function scope.
-console.log(variable3);
+// console.log(variable3);
 // -> it gives undefined value here 
-var variable3 = "saksham";
-console.log(variable3);
-// -> it give saksham
+// var variable3 = "saksham";
+// console.log(variable3);
+// // -> it give saksham
 
-console.log(varaible4);
-// -> cannot access before initialize
-let varaible4 = "saksham";
-console.log(varaible4);
-// -> saksham
+// console.log(varaible4);
+// // -> cannot access before initialize
+// let varaible4 = "saksham";
+// console.log(varaible4);
+// // -> saksham
 
 
 
