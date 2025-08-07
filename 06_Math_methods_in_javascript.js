84d187828473ebddc9ef5fd0cbff0ddcf8f19844
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2025-08-07 13:14:29 +0530

    update in 6 or 7
    completed 6th chapter and 7th chapter

--- 06_Math_methods_in_javascript.js
+++ 06_Math_methods_in_javascript.js
@@ -51,3 +51,51 @@
     // console.log(Math.floor(4)); output: 4
     // console.log(Math.floor(-4.9)); output: -5
     // console.log(Math.floor(-4.1)); output: -5
+
+// 4.Math.ceil(x);
+    // Round UP to the nearest whole number (integer), even if the decimal part is very small.
+    //    console.log(Math.ceil(4.3)); output:5
+    //    console.log(Math.ceil(6.0)); output:6
+    //    console.log(Math.ceil(-4.3)); because -4 is greater than -4.3
+
+// 5.Math.round(x);
+// for positive
+    // console.log(Math.round(4.4)); output:4
+    // console.log(Math.round(4.5)); output:5
+    // console.log(Math.round(4.6)); output:5
+
+// for negative
+    // console.log(Math.round(-4.4)); output:-4
+    // console.log(Math.round(-4.5)); output:-4 (roundsup towared negativly)
+    // console.log(Math.round(-4.6)); output:-5
+
+
+    // 5.Math.random()
+    // 1.console.log(Math.random()); gives random number between 0 and 1 not including the 1 in it .
+
+    // 2. let num = Math.random() *10;
+    // console.log(num); gives a decimal number between 0 and 10 not including the 10 .
+
+    // 3. let num = Math.floor(Math.random()*10);
+    // console.log(num);  gives a whole number between 0 to 10 not including the 10.
+
+    // 4. let randomInt = Math.floor(Math.random()*10)+1;
+    // console.log(randomInt); gives a whole number between 0 to 10 including the 10.
+
+    // 5. function getRandomNumber(min,max){
+    //         return Math.floor(Math.random()*(max-min+1)) + min; 
+    // }
+    // the range here is 5, 15 so after - it gives 10 , after adding +1 the range is 11 so we have to add the min which is 5 
+    // to strech the length and make it to 15 so now total max made is 16 now we can generate from 5 to 16 not including 16.
+    // let number = getRandomNumber(5,100);
+    // console.log(number);
+
+
+
+    // Infinity
+    
+    // let num  = 8/0 ; output: Infinity;
+    // let num  = -8/0 ; output : -Infinity;
+    // typeof Infinity ; output : Number;
+    // let num = 0/0; output : NaN;
+    // typeof NaN; output :Number;
---
+++ 07_Truthy_and_falsy.js
@@ -0,0 +1,16 @@
+// Boolean(10); -> true
+// Boolean(1); -> 1
+// Boolean(0); -> false
+// Boolean(-0);->false
+// Boolean(-1);->true
+// Boolean(Infinity); -> true
+// Boolean(NaN); -> false
+// console.log(Boolean(NaN));
+
+// Boolean(""); -> false , cause it is empty String.
+// Boolean("   "); -> true , cause it is not a empty String.
+// Boolean(undefined);-> false 
+
+// all the values except are all truthy. 
+
+// now concluding all the falsy values are -> 0, -0, NaN, '',undefined , null
